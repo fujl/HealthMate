b110220840a7eee34d672beba05779d97f2d8e3a
fujl <jialin.fu@longmaster.com.cn>
2017-08-11 13:33:26 +0800

    提交代码

---
+++ HealthMate/app/src/main/java/com/mobile/healthmate/app/lib/listener/SimpleAnimatorListener.java
@@ -0,0 +1,29 @@
+package com.mobile.healthmate.app.lib.listener;
+
+import android.animation.Animator;
+
+/**
+ * Created by Lxx on 2017/1/19.
+ */
+
+public class SimpleAnimatorListener implements Animator.AnimatorListener {
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+}
---
+++ HealthMate/app/src/main/java/com/mobile/healthmate/app/lib/listener/SimpleTextWatcher.java
@@ -0,0 +1,25 @@
+package com.mobile.healthmate.app.lib.listener;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Created by Lxx on 2017/1/6.
+ */
+
+public class SimpleTextWatcher implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
